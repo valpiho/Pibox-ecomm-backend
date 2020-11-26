afa8d37e898e71c902f1e1e1bcfa22863b923994
Valentin Piho <val.piho@gmail.com>
2020-11-26 15:00:26 +0200

    Entities and REST for Countries and States

--- src/main/java/com/pibox/ecom/config/MyDataRestConfig.java
+++ src/main/java/com/pibox/ecom/config/MyDataRestConfig.java
@@ -1,8 +1,13 @@
 package com.pibox.ecom.config;
 
+import com.pibox.ecom.entity.Country;
+import com.pibox.ecom.entity.Product;
+import com.pibox.ecom.entity.ProductCategory;
+import com.pibox.ecom.entity.State;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import javax.persistence.EntityManager;
@@ -19,10 +24,23 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        disableHttpMethod(Product.class, config, theUnsupportedActions);
+        disableHttpMethod(ProductCategory.class, config, theUnsupportedActions);
+        disableHttpMethod(Country.class, config, theUnsupportedActions);
+        disableHttpMethod(State.class, config, theUnsupportedActions);
 
         config.exposeIdsFor(entityManager
                 .getMetamodel()
                 .getEntities()
                 .stream().map(Type::getJavaType).toArray(Class[]::new));
+    }
+
+    private void disableHttpMethod(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+        config.getExposureConfiguration()
+                .forDomainType(theClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions)));
     }
 }
---
+++ src/main/java/com/pibox/ecom/dao/StateRepository.java
@@ -0,0 +1,16 @@
+package com.pibox.ecom.dao;
+
+import com.pibox.ecom.entity.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.Set;
+
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource()
+public interface StateRepository extends JpaRepository<State, Integer> {
+
+    Set<State> findByCountryCode(@Param("code") String code);
+}
